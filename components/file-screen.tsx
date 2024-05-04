04ceceebd8c587a33d045e8a25bff97194f7c673
itinnove <itinnove@mbp-de-itinnove.home>
2024-05-04 16:33:56 +0200

    changed files ui

--- components/file-screen.tsx
+++ components/file-screen.tsx
@@ -1,9 +1,8 @@
 import { DocFromDb, ResponseDoc } from "@/lib/types/types";
 import { readTextFile } from "@tauri-apps/api/fs";
 import { useRef, useState } from "react";
-import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ListCollapse, Loader2, SquarePlus } from "lucide-react";
+import { FileMinus, ListCollapse, Loader2, RefreshCcw, SquarePlus, Trash2 } from "lucide-react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { open } from "@tauri-apps/api/dialog";
 import { FilesTable } from "./ui/datatable";
@@ -11,8 +10,7 @@ import { useToast } from "./ui/use-toast";
 
 export default function FilesScreen(
     { DB, reloadDB }: { DB: DocFromDb[]; reloadDB: () => void },
-) {
-    const [uploadedDocuments, setUploadedDocuments] = useState<DocFromDb[]>([]);
+) {    
     const [text, setText] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
     const { toast } = useToast();
@@ -33,14 +31,14 @@ export default function FilesScreen(
               const data = await readTextFile(selectedPaths[i] as string);                         
               documents.push({content: data, path: selectedPaths[i] as string});
           }
-          setUploadedDocuments(documents);
+          createEmbeddings(documents);        
         } catch (err) {
           console.error(err);
         }
     };
 
     const splitDocumentsIntoWordChunks = (
-        documents: {path: string, content: string}[],
+        documents: DocFromDb[],
         chunkSize: number,
     ): {content: string, path: string}[] => {
         let chunks: {content: string, path: string}[] = [];
@@ -68,11 +66,11 @@ export default function FilesScreen(
         return chunks;
     };
 
-    const createEmbeddings = async () => {
+    const createEmbeddings = async (documents: DocFromDb[]) => {
         setLoading(true);
 
         const chunkSize = 256;
-        const chunks = splitDocumentsIntoWordChunks(uploadedDocuments, chunkSize);
+        const chunks = splitDocumentsIntoWordChunks(documents, chunkSize);
 
         toast({
             title: "Started embedding your files...",
@@ -92,15 +90,15 @@ export default function FilesScreen(
           });
     }
 
+    const notImlementedYet = () => {
+        return alert("Not implemented now...")
+    }
+
     return (
         <div className="w-screen h-full px-4">
-            <Textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className="resize-none w-full min-h-[300px]"
-                placeholder="Enter some text that will be used to create you database"
-            />
-            <div className="flex flex-row w-full justify-center space-x-4 my-4">
+            <h2 className="text-muted-foreground font-extrabold text-lg pt-4">Your files</h2>
+            <p className="text-muted-foreground text-sm mb-4">You can select .txt files and put them to the app, all your files will stay on your computer, nothing will be uploaded to the cloud.<br /><b>Your files, your space : store locally, access freely :)</b></p>
+            <div className="flex flex-row w-full justify-center space-x-4 max-w-[800px] mx-auto">
                 <Button
                     variant="secondary"
                     className="w-full"
@@ -112,24 +110,38 @@ export default function FilesScreen(
                         ? <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                         : <SquarePlus className="h-4 w-4 mr-2 text-muted-foreground" />}
                     <p className="text-muted-foreground">Add files</p>
-                </Button>                
+                </Button>
                 <Button
-                    variant="secondary"
-                    className="w-full"
-                    onClick={createEmbeddings}
+                    variant="outline"                    
+                    onClick={notImlementedYet}
                     disabled={loading}
                 >
                     {loading
-                        ? <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                        : <SquarePlus className="h-4 w-4 mr-2 text-muted-foreground" />}
-                    <p className="text-muted-foreground">Upload to the DB</p>
-                </Button>                
+                        ? <Loader2 className="h-4 w-4 mr-2 animate-spin" />
+                        : <FileMinus className="h-4 w-4 mr-2 text-muted-foreground" />}
+                        <p className="text-muted-foreground">Remove files</p>                 
+                </Button>
+                <Button
+                    variant="outline"                    
+                    onClick={notImlementedYet}
+                    disabled={loading}
+                >
+                    {loading
+                        ? <Loader2 className="h-4 w-4 mr-2 animate-spin" />
+                        : <RefreshCcw className="h-4 w-4 mr-2 text-muted-foreground" />}
+                        <p className="text-muted-foreground">Reload db</p>                 
+                </Button>
+                <Button
+                    variant="outline"                    
+                    onClick={notImlementedYet}
+                    disabled={loading}
+                >
+                    {loading
+                        ? <Loader2 className="h-4 w-4 mr-2 animate-spin" />
+                        : <Trash2 className="h-4 w-4 mr-2 text-muted-foreground" />}
+                        <p className="text-muted-foreground">Delete db</p>                    
+                </Button>           
             </div>
-            add files
-            remove files
-            reload whole db
-            delete whole db
-            see file in finder
             <FilesTable DB={DB.slice(0, 10)} />
         </div>
     );
